a748984410c169e858229a33b75a87346963e211
David Onuh <davidonuh1@gmail.com>
2021-03-06 02:08:58 +0100

    change create view to classbased view

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, Response, json, make_response, jsonify
+from flask.views import MethodView
 from bson import json_util
 from models.db import initialize_db
 from models.models import Song, Podcast, AudioBook
@@ -20,36 +21,39 @@ def get_audios():
     audios = Podcast.objects().to_json()
     return Response(audios, mimetype="application/json", status=200)
 
-
-@app.route('/create', methods=["POST"])
-def create_audio():
-    a_type = request.json['audioFileType']
+class CreateAudiosView(MethodView):
+    def post(self):
+        a_type = request.json['audioFileType']
     
-    if a_type == "song":
-        response = create_song(request.json['song_title'], request.json['song_duration'])
-        return make_response(json.loads(json_util.dumps(response)), 200)
-    
-    elif a_type == "podcast":
-
-        response = create_podcasts(
-            podcast_name=request.json['podcast_name'],
-            podcast_duration=request.json['podcast_duration'],
-            host=request.json['host'], participants=request.json['participants']
-            )
+        if a_type == "song":
+            response = create_song(request.json['song_title'], request.json['song_duration'])
+            return make_response(json.loads(json_util.dumps(response)), 200)
         
-        return make_response(json.loads(json_util.dumps(response)), 200)
-    
-    elif a_type == "audiobook":
-        response = create_audiobooks(
-            title=request.json['title'],
-            duration=request.json['duration'],
-            author=request.json['author'], narrator=request.json['narrator']
-        )
-        return make_response(json.loads(json_util.dumps(response)), 200)
-    else:
-        #raise error
-        return make_response(jsonify({"error": "Internal Server Error"}), 500)
+        elif a_type == "podcast":
 
+            response = create_podcasts(
+                podcast_name=request.json['podcast_name'],
+                podcast_duration=request.json['podcast_duration'],
+                host=request.json['host'], participants=request.json['participants']
+                )
+            
+            return make_response(json.loads(json_util.dumps(response)), 200)
+        
+        elif a_type == "audiobook":
+            response = create_audiobooks(
+                title=request.json['title'],
+                duration=request.json['duration'],
+                author=request.json['author'], narrator=request.json['narrator']
+            )
+            return make_response(json.loads(json_util.dumps(response)), 200)
+        else:
+            #raise error
+            return make_response(jsonify({"error": "Internal Server Error"}), 500)
+
+
+
+
+app.add_url_rule('/audio/create/', view_func=CreateAudiosView.as_view('create_audio_song_podcast_audiobook'))
 
 # Run Server
 if __name__ == "__main__":
